ba03a34a8ca72bd25d096c4f3d4babaacec60b89
FCAniche <aneesh.singh.bais@gmail.com>
2023-07-02 18:15:03 -0500

    Added datetime of server

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from markupsafe import escape
+from datetime import datetime
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,11 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index/')
 def hello():
-    return '<h1>Hello, World!</h1>'
+    return '<h1>Hello World! Welcome to S14A2023</h1>'
 
-@app.route('/about/')
+@app.route('/datetime/')
 def about():
-    return '<h3>This is a Flask web application.</h3>'
+    return '<h3>Server datetime: {}</h3>'.format(str(datetime.now()))
 
 @app.route('/capitalize/<word>/')
 def capitalize(word):
